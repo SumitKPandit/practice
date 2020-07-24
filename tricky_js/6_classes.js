74f3dfcd58af3b982791ffccad6b5042f60c2bae
Sumit Kumar Pandit <sumit.pandit@gds.ey.com>
2020-07-24 13:36:14 +0530

    tricky js

--- tricky_js/6_classes.js
+++ tricky_js/6_classes.js
@@ -11,9 +11,20 @@ class Book {
         const years = new Date().getFullYear() - this.year;
         return `${this.title} is ${years} years old.`;
     }
+    revise(newYear) {
+        this.year = newYear;
+        this.revised = true;
+    }
+    static topBookStore() {
+        return "Barnes & Noble";
+    }
 }
 
 // Instantiate Object
 const book1 = new Book("Book One", "John Doe", "2013");
 
-console.log(book1);
+// console.log(book1);
+// book1.revise("2018");
+// console.log(book1);
+
+console.log(Book.topBookStore());
